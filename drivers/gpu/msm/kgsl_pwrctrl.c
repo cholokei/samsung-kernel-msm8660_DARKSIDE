367e6462d9139e2e6a6dbf585c32d161df49e45f
TheDerekJay <thederekjay@gmail.com>
2012-06-28 21:47:28 -0400

    drivers: cpufreq: Add Badass CPU Governor
    Conflicts:
    
    	drivers/cpufreq/Kconfig

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -20,6 +20,8 @@
 #include "kgsl_device.h"
 #include "kgsl_trace.h"
 
+#include <linux/cpufreq.h>
+
 #define KGSL_PWRFLAGS_POWER_ON 0
 #define KGSL_PWRFLAGS_CLK_ON   1
 #define KGSL_PWRFLAGS_AXI_ON   2
@@ -28,6 +30,10 @@
 #define GPU_SWFI_LATENCY	3
 #define UPDATE_BUSY_VAL		1000000
 #define UPDATE_BUSY		50
+
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
+extern bool gpu_busy_state;
+#endif
 
 struct clk_pair {
 	const char *name;
@@ -343,6 +349,13 @@ static void kgsl_pwrctrl_busy_time(struct kgsl_device *device, bool on_time)
 		b->time = 0;
 	}
 	do_gettimeofday(&(b->start));
+	
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL	
+  if (on_time)
+    gpu_busy_state = true;
+  else	
+    gpu_busy_state = false;		
+#endif
 }
 
 void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
